b07d483603bd06d4f009db270312793601189832
jingfei <jingfei955047@gmail.com>
2017-10-26 17:10:09 +0800

    Make field dynamic.

--- js/home.js
+++ js/home.js
@@ -1,8 +1,32 @@
 (function() {
+	dynamicWidth();
 })();
 
-document.querySelector('#arc').setAttribute('d',describeArc(700,565,200,-73.5,73.5));
-	
+window.onresize = function(event) { dynamicWidth(); };
+
+function dynamicWidth() {
+	const width = window.innerWidth
+				|| document.documentElement.clientWidth
+				|| document.body.clientWidth,
+		  height = window.innerHeight
+				|| document.documentElement.clientHeight
+				|| document.body.clientHeight,
+		  svg = document.querySelector('svg');
+	svg.setAttribute('width', width);
+	svg.setAttribute('height', height);
+	buildField(svg,width/10, width*.5, height*.8);
+}
+
+function buildField(svg, edge, centerX, centerY) {
+	const pitcherR = edge * .1875 * .5, pitcherDis = edge * .675;
+	svg.innerHTML = `
+	<rect id="square" x="${centerX-edge}" y="${centerY-edge}" width="${edge}" height="${edge}"/>
+			<line id="left-line" x1="${centerX}" y1="${centerY}" x2="${centerX-edge*3}" y2="${centerY-edge*3}"/>
+			<line id="right-line" x1="${centerX}" y1="${centerY}" x2="${centerX+edge*3}" y2="${centerY-edge*3}"/>
+			<circle id="pitcher" r="${pitcherR}" cx="${centerX}" cy="${centerY-pitcherDis}" />
+			<path id="arc" d="${describeArc(centerX,centerY-pitcherDis,edge,-73.5,73.5)}">`;
+}
+
 function polarToCartesian(centerX, centerY, radius, angleInDegrees) {
   var angleInRadians = (angleInDegrees-90) * Math.PI / 180.0;
 
