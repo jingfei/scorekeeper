e8d47c2d0b8641bb36091ea31a1246c62cacdc87
jingfei <jingfei955047@gmail.com>
2018-10-12 17:52:25 +0800

    Add runner logic.

--- src/app/runners.ts
+++ src/app/runners.ts
@@ -1,11 +1,63 @@
 export class Runners {
   /* location from index to where? 
    * size fix 4
+   * -1 stands for no runner
    */
   location: number[],
   score: number;
 
-  constructor(values: Object = {}) {
-    Object.assign(this, values);
+  constructor(last: Runners = null) {
+    this.location.push(0);
+    this.location.push(-1);
+    this.location.push(-1);
+    this.location.push(-1);
+
+    if (last !== null) {
+      for (int i = 0; i < 4; ++i) {
+        setRunner(last.location[i])
+      }
+    }
+  }
+
+  isValidIndex(index: number): boolean {
+    return index < 4 && index >= 0;
+  }
+
+  find(base: number): number { // find index of runner
+    for (int i = 0; i < 4; ++i) {
+      if (this.location[i] === base) {
+        return i;
+      }
+    }
+    return -1;
+  }
+
+  go(base: number, step: number = 1) {
+    index = this.find(base);
+    if (index !== -1) {
+      this.location[index] += step;
+      if (this.location[index] > 3) {
+        ++this.score;
+      }
+    }
+  }
+
+  force(base: number = 0) { // forced base from batter
+    if(this.isValidIndex(base)) {
+      if (this.hasRunner(base + 1)) {
+        this.force(base + 1);
+      }
+      go(base);
+    }
+  }
+
+  hasRunner(base: number): boolean { // find if base had runner
+    return this.find(base) !== -1;
+  }
+
+  setRunner(index: number) {
+    if (this.isValidIndex(index)) {
+      this.location[index] = index;
+    }
   }
 }
